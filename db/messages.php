aca65758bec615391245155b207ff14ae7fedda9
Amanda Doughty <amanda.doughty@synergy-learning.com>
2024-11-09 16:02:02 +0000

    Fix for: 4.5 removes some deprecated messaging constants, causes install error #25 provided by davidscotson

--- db/messages.php
+++ db/messages.php
@@ -29,8 +29,8 @@ $messageproviders = array(
     // Grade released.
     'grade_released' => [
         'defaults' => [
-            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
-            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDOFF,
+            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
+            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
         ],
     ],
 
